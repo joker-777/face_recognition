a2f401cee29e50846b529ab84aff37c7be3bee43
JoKer <joker777vs.666@gmail.com>
2017-06-22 23:37:50 +0200

    added cross-fading

--- js/main.js
+++ js/main.js
@@ -3,12 +3,31 @@ function initExample() {
 
     var webcam		= document.getElementById("_webcam");		// our webcam video
     var imageData	= document.getElementById("_imageData");	// image data for BRFv4
-    var faceData	= document.getElementById("_faceData");
+    var canvas	    = document.getElementById("_faceData");
+    var canvas2	    = document.getElementById("_faceData2");
+
+    // var height      = $(canvas).height();
+    // var width       = $(canvas).width();
+    var height      = canvas.height;
+    var width       = canvas.width;
 
     var brfManager	= null;
     var resolution	= null;
     var brfv4		= null;
 
+	var imageDataCtx = imageData.getContext("2d");
+	var context = canvas.getContext("2d");
+	context.fillStyle = '#000000';
+	context.fillRect(0, 0, width, height)
+	var context2 = canvas2.getContext("2d");
+	context2.fillStyle = '#000000';
+	context2.fillRect(0, 0, width, height)
+
+	// variables para cross-fade
+	var len = 4 * width * height;
+	var offset = new Array(len);
+	var delta = new Array(len);
+ 
     startCamera();
 
     waitingPromse = $.Deferred().resolve()
@@ -57,8 +76,8 @@ function initExample() {
         // Resize the canvas to match the webcam video size.
         imageData.width		= webcam.videoWidth;
         imageData.height	= webcam.videoHeight;
-        faceData.width		= webcam.videoWidth;
-        faceData.height	    = webcam.videoHeight;
+        // canvas.width		= webcam.videoWidth;
+        // canvas.height	= webcam.videoHeight;
 
         resolution	= new brfv4.Rectangle(0, 0, imageData.width, imageData.height);
 
@@ -69,12 +88,32 @@ function initExample() {
         setInterval(trackFaces, 1000/30);
     }
 
+
+    // https://github.com/ariya/X2/blob/master/javascript/crossfading/crossfading.js
+    function tween( result, factor) {
+        var i, r;
+        r = result.data;
+        for (i = 0; i < len; i += 4) {
+            r[i] = offset[i] + delta[i] * factor;
+            r[i + 1] = offset[i + 1] + delta[i + 1] * factor;
+            r[i + 2] = offset[i + 2] + delta[i + 2] * factor;
+        }
+        context.putImageData(result, 0, 0);
+    }
+
+    function startFading(result) {
+        value = 1;
+        ticker = window.setInterval(function () {
+            value -= 0.01;
+            tween(result, value);
+            if (value <= 0) { window.clearInterval(ticker); }
+        }, 1000 / 60);
+    }
+
     function trackFaces() {
 
         if (waitingPromse.state() == 'pending') { return; }
 
-        var imageDataCtx = imageData.getContext("2d");
-        var faceDataCtx = faceData.getContext("2d");
 
         // imageDataCtx.setTransform(-1.0, 0, 0, 1, resolution.width, 0); // mirrored for draw of video
         imageDataCtx.drawImage(webcam, 0, 0, resolution.width, resolution.height);
@@ -93,17 +132,36 @@ function initExample() {
 
             var face = faces[0];
 
-            console.log('face tracked');
-
-            dh = resolution.height
-            dw = face.width * (resolution.height/face.height)
+            dh = height
+            dw = face.width * (height/face.height)
             dy = 0
-            dx = (resolution.width - face.width) / 2
+            dx = Math.abs(width - dw) / 2
 
-            faceDataCtx.drawImage(webcam, face.x, face.y, face.width, face.height, dx, dy, dw, dh);
+            debugger
+
+            // canvas.style.opacity = 0;
+
+            source = context.getImageData(0, 0, width, height);
+            context2.drawImage(webcam, face.x, face.y, face.width, face.height, dx, dy, dw, dh);
+            target = context2.getImageData(0, 0, width, height);
+            result = context.createImageData(width, height)
+
+            for (i = 0; i < len; i += 1) {
+                offset[i] = target.data[i];
+                delta[i] = source.data[i] - target.data[i];
+                result.data[i] = 255;
+            }
+
+            // context.fillStyle = '#fff'
+            // context.fillRect(0, 0, resolution.width, resolution.height);
+
+            // canvas.style.opacity = 1;
+
+			startFading(result);
 
             waitingPromse = $.Deferred();
             setTimeout(function() { waitingPromse.resolve() }, 2000)
+
 
             // if(face.state === brfv4.BRFState.FACE_TRACKING_START ||
             //     face.state === brfv4.BRFState.FACE_TRACKING) {
